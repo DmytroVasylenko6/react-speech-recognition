9ed056ea613ac54e3549f1bf5a8c35c2859aa6e7
DmytroVasylenko6 <tredstoun651@ukr.net>
2022-12-14 12:03:17 +0200

    fix files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import Dictaphone from './Components/Dictaphone';
-import CaptionsRaw from './Components/CaptionsRaw';
+// import CaptionsRaw from './Components/CaptionsRaw';
 import './App.css';
 
-const captions = `
-WEBVTT
+// const captions = `
+// WEBVTT
 
-00:00.000 --> 00:02.000
-This is the first two seconds
+// 00:00.000 --> 00:02.000
+// This is the first two seconds
 
-00:02.000 --> 00:04.000
-This is the second two seconds
-This is multiple lines
-Another
+// 00:02.000 --> 00:04.000
+// This is the second two seconds
+// This is multiple lines
+// Another
 
-00:05.000 --> 00:06.000
-This is the third caption
-`.trim();
+// 00:05.000 --> 00:06.000
+// This is the third caption
+// `.trim();
+
 
 function App() {
   return (
